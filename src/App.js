1409ff43a8fbc70d164ebe963023bba925ff17fe
Mahi Al Akhnas <mahialakhnas@fantacode.com>
2023-10-07 19:27:11 +0530

    one

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './Components/Header/Header'
+import Loader from './Components/Loader/Loader'
+import Login from './Pages/Login/Login';
+
+function App() {
+  return (
+    <div className="App">
+      <Login/>
+      {/* <Loader/> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Loader/Loader.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Loader.css'
+
+function Loader() {
+  return (
+    <div className="center">
+  <div className="wave"></div>
+  <div className="wave"></div>
+  <div className="wave"></div>
+  <div className="wave"></div>
+  <div className="wave"></div>
+  <div className="wave"></div>
+  <div className="wave"></div>
+  <div className="wave"></div>
+  <div className="wave"></div>
+  <div className="wave"></div>
+</div>
+  )
+}
+
+export default Loader
